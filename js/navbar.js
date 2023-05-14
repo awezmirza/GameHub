56d4a4a0f858c15a823d9114afcaf8727cd033de
Awez <awezbaigmirza@gmail.com>
2023-05-14 14:58:38 +0530

    First commit

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     $("#navbar-container").load("navbar.html");
+//   });
+  
+$(document).ready(function() {
+  $("#navbar-container").load("navbar.html", function() {
+    // Callback function executed after the HTML file is loaded
+
+    // Get the MenuIcon element within the loaded content
+    const menuIcon = $('#MenuIcon');
+
+    // Add a click event listener to the MenuIcon
+    menuIcon.on('click', function() {
+      // Toggle the "active" class on the MenuWrap element
+      $('#MenuWrap').toggleClass('active');
+    });
+  });
+});
---
+++ js/sidebarscript.js
@@ -0,0 +1,21 @@
+// We are importing the elements from navbar.html because it is not in main html  
+$(document).ready(function() {
+  $("#navbar-container").load("navbar.html", function() {
+
+    const menuIcon = $('.MenuIcon');
+
+    // Add a click event listener to the MenuIcon
+    menuIcon.on('click', function() {
+      // Toggle the "active" class on the MenuWrap element
+      $('#MenuWrap').toggleClass('active');
+    });
+  });
+});
+ 
+// -----------------------------------------------------------------------------------------
+
+
+
+
+
+
