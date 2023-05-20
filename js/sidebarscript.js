2baebb0f8cb330b843ac27066c5f264adc4febb3
Awez <awezbaigmirza@gmail.com>
2023-05-20 13:45:53 +0530

    Added Many more features

--- js/sidebarscript.js
+++ js/sidebarscript.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
   $("#navbar-container").load("navbar.html", function() {
 
     const menuIcon = $('.MenuIcon');
-
     // Add a click event listener to the MenuIcon
     menuIcon.on('click', function() {
       // Toggle the "active" class on the MenuWrap element
       $('#MenuWrap').toggleClass('active');
+      $('#gamescontainer').toggleClass('active');
+    });
+
+    const togglecoloricon = $('.togglecoloricon');
+    togglecoloricon.on('click', function() {
+      $('.togglecoloricon').toggleClass('onclick');
     });
   });
 });
