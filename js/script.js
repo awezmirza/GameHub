2bdd829bee131d4941ed0bd1226d19a34e61ce1b
Awez <awezbaigmirza@gmail.com>
2023-05-25 01:45:06 +0530

    Basic Updates

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Get the Sign Out button element
-const signOutButton = document.getElementById('SignOutButton');
-
-// Add a click event listener to the Sign Out button
-signOutButton.addEventListener('click', function () {
-  // Redirect to index.html
-  window.location.href = 'index.html';
-});
